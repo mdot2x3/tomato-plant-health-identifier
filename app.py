2ba1ea8439f394b1bf3c18ced38594a64016b39d
mdot2x3 <mdot2x3@gmail.com>
2025-08-18 20:57:36 -0700

    ops: modify port for Heroku deployment
    change port to listen on the port specified by the
    $PORT environment variable and on all interfaces
    (0.0.0.0) in app.py.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from PIL import Image
 import numpy as np
 import io
 from tflite_runtime.interpreter import Interpreter
+import os
 
 app = Flask(__name__)
 
@@ -71,4 +72,5 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
